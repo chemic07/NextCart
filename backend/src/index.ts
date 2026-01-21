2177aac44198b65511aeb2709bdcda8275d3708b
chemic07 <rajlodhi1208@gmail.com>
2026-01-21 21:37:41 +0530

    Fix PORT type for app.listen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 await connectDB();
 
@@ -9,4 +9,6 @@ app.get("/", (_req, res) => {
   res.send("API is running...");
 });
 
-app.listen(PORT, () => console.log("Running on", PORT));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Running on", PORT);
+});
